87d07dd2ed7d8a26cb0b8b35a61820c4e601b6d9
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-22 16:39:21 +0200

    add path to login, logout, register

---
+++ src/controllers/userController.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+
+const login = (req, res) => {
+    res.json({msg: 'logged'});
+}
+
+const register = (req, res) => {
+    res.json({msg: 'registred'});
+
+}
+
+const logout = (req, res) => {
+    res.json({msg: 'loggedout'});
+
+}
+
+router.post('/login', login);
+router.post('/register', register);
+router.post('/logout', logout);
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-const app = require('express')
+const express = require('express');
+const app = express();
+const routes = require('./routes')
+
+const port = 5000;
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(port, () => console.log(`Server is running on ${port}`));
