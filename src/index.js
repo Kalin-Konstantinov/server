be44f93d0cdf239de21c6cccc0d11060f8c946bc
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-22 17:03:09 +0200

    install and setup cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
-const routes = require('./routes')
+const cors = require('cors');
+const routes = require('./routes');
+
+
 
 const port = 5000;
 
 app.use(express.json());
-
+app.use(cors());
 app.use(routes);
 
 app.listen(port, () => console.log(`Server is running on ${port}`));
