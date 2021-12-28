12287b647bc99babbba7b18fb72aa079fa913cd4
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-28 19:52:45 +0200

    add guard to postCategory only admin access

--- src/controllers/categoryController.js
+++ src/controllers/categoryController.js
@@ -4,6 +4,11 @@ const router = require('express').Router();
 
 
 const postCategory = (req, res) => {
+
+    if (req.user.email !== 'admin@admin') {
+        return req.json({ err: 403, message: 'Access to the requested resource is forbidden!' })
+    }
+
     let { name, imageUrl } = req.body;
     name = name.toLowerCase();
     const data = { name, imageUrl };
