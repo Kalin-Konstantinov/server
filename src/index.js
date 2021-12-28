a0d68471724ab22b21ea71fb88118b7f29ac07c7
Kalin-Konstantinov <kalink.wow@gmail.com>
2021-12-28 15:47:50 +0200

    add compass as db

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 
 const routes = require('./routes');
 const { checkAndSetUser } = require('./middlewares/authMiddlewares');
+const { user } = require('../dbPasswordAndUsername');
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.use(checkAndSetUser)
 app.use(cors());
 app.use(routes);
 
-mongoose.connect('mongodb://localhost:27017/react-project')
+mongoose.connect(`mongodb+srv://${user.username}:${user.password}@cluster0.fmnru.mongodb.net/react-application?retryWrites=true&w=majority`)
     .then(() => {
         console.log('DB is connected!');
         app.listen(port, () => console.log(`Server is running on port 5000`));
